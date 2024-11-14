deb889bbed563a4aaad0ca83bffafe964fa9e920
kumudu-dodandeniya <69890196+kumudu-dodandeniya@users.noreply.github.com>
2024-11-14 21:13:02 +0530

    create header section

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,10 +1,19 @@
 import React from 'react'
 import Nevbar from './components/Nevbar/Nevbar'
+import { Route, Routes } from 'react-router-dom'
+import Home from './pages/Home/Home'
+import PlaceOrder from './pages/PlaceOrder/PlaceOrder'
+import Cart from './pages/Cart/Cart'
 
 const App = () => {
   return (
     <div className='app'>
     <Nevbar> </Nevbar>
+    <Routes>
+      <Route path='/' element={<Home/>} />
+      <Route path='/cart' element={<Cart/>} />
+      <Route path='/order' element={<PlaceOrder/>}/>
+    </Routes>
     </div>
   )
 }
---
+++ frontend/src/pages/Home/Home.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import './Home.css'
+import Header from '../../components/Header/Header'
+
+const Home = () => {
+  return (
+    <div>
+      <Header>
+        
+      </Header>
+
+    </div>
+  )
+}
+
+export default Home
