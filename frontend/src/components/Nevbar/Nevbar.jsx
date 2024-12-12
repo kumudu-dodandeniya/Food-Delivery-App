d00a49c5b89aa3e4c311475896a48f020cdf4df1
kumudu-dodandeniya <69890196+kumudu-dodandeniya@users.noreply.github.com>
2024-12-12 12:22:34 +0530

    create cart

--- frontend/src/components/Nevbar/Nevbar.jsx
+++ frontend/src/components/Nevbar/Nevbar.jsx
@@ -20,7 +20,7 @@ const Nevbar = ({setShowLogin}) => {
     <div className="navbar-right">
       <img src={assets.search_icon} alt="" />
       <div className="navbar-search-icon">
-        <img src={assets.basket_icon} alt="" />
+        <Link to={'/cart'}> <img src={assets.basket_icon} alt="" /> </Link>
         <div className="dot"></div>
       </div>
       <button onClick={()=>setShowLogin(true)}>sign in</button>
--- frontend/src/pages/Cart/Cart.jsx
+++ frontend/src/pages/Cart/Cart.jsx
@@ -1,9 +1,28 @@
-import React from 'react'
+import React, { useContext } from 'react'
 import './Cart.css'
+import { StoreContext } from '../../context/StoreContext';
 
 const Cart = () => {
+
+  const{cartItem, food_list, removeCart} = useContext(StoreContext);  
+  
   return (
-    <div>Cart</div>
+    <div className='cart'>
+      <div className="cart-item">
+        <div className="cart-item-title">
+          <p>Items</p>
+          <p>Title</p>
+          <p>Price</p>
+          <p>Quentity</p>
+          <p>Total</p>
+          <p>Remove</p>
+        </div>
+        <br />
+        <hr />
+      </div>
+
+
+    </div>
   )
 }
 
