cdbf1759708c019a456cc8f9e1ead62bb24251fa
kumudu-dodandeniya <69890196+kumudu-dodandeniya@users.noreply.github.com>
2024-12-12 17:01:06 +0530

    update6

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,7 +1,17 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
+/*
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+})*/
+
+
+export default defineConfig({
+  plugins: [react()],
+  base: '/', // Ensures proper base path; change to '/subdirectory/' if deploying under a subdirectory
+  build: {
+    outDir: 'dist', // Default output directory
+  },
+});
