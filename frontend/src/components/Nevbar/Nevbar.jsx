2c8073013a10b86f4bcf18aa9bd8febe35561394
kumudu-dodandeniya <69890196+kumudu-dodandeniya@users.noreply.github.com>
2025-01-03 12:34:23 +0530

    create cart item

--- frontend/src/components/Nevbar/Nevbar.jsx
+++ frontend/src/components/Nevbar/Nevbar.jsx
@@ -1,16 +1,19 @@
-import React, { useState } from 'react'
+import React, { useContext, useState } from 'react'
 import './Nevbar.css'
 import { assets } from '../../assets/assets'
 import { Link } from 'react-router-dom'
+import { StoreContext } from '../../context/StoreContext'
 
 const Nevbar = ({setShowLogin}) => {
  //state
   const [menu,setMenu] = useState("Home")
 
+  const {getTotalCartAmount} = useContext(StoreContext);
+
 
   return (
     <div className='navbar'>
-    <img src={assets.logo } alt="" className='logo' />
+    <Link to='/'><img src={assets.logo } alt="" className='logo' /></Link>
     <ul className="navebar-manu">
       <Link to= '/' onClick={()=>setMenu("Home")} className={menu=== "Home"?"active":""}>Home</Link>
       <a href="#explore-menu"onClick={()=>setMenu("Manu")} className={menu=== "Manu"?"active":""}>Manu</a>
@@ -21,7 +24,7 @@ const Nevbar = ({setShowLogin}) => {
       <img src={assets.search_icon} alt="" />
       <div className="navbar-search-icon">
         <Link to={'/cart'}> <img src={assets.basket_icon} alt="" /> </Link>
-        <div className="dot"></div>
+        <div className={getTotalCartAmount()===0?"":"dot"}></div>
       </div>
       <button onClick={()=>setShowLogin(true)}>sign in</button>
     </div>
--- frontend/src/pages/Cart/Cart.jsx
+++ frontend/src/pages/Cart/Cart.jsx
@@ -1,15 +1,17 @@
 import React, { useContext } from 'react'
 import './Cart.css'
 import { StoreContext } from '../../context/StoreContext';
+import { useNavigate } from 'react-router-dom';
 
 const Cart = () => {
 
-  const{cartItem, food_list, removeCart} = useContext(StoreContext);  
+  const{cartItems, food_list, removeCart, getTotalCartAmount} = useContext(StoreContext);  
   
+  const navigate = useNavigate()
   return (
     <div className='cart'>
-      <div className="cart-item">
-        <div className="cart-item-title">
+      <div className="cart-items">
+        <div className="cart-items-title">
           <p>Items</p>
           <p>Title</p>
           <p>Price</p>
@@ -19,6 +21,66 @@ const Cart = () => {
         </div>
         <br />
         <hr />
+        {food_list.map ((item,index)=>{
+          if(cartItems[item._id]>0)
+          {
+            return(
+              <div>
+                <div className="cart-items-title cart-items-item">
+                  <img src={item.image} alt="" />
+                  <p>{item.name}</p>
+                  <p>Rs.{item.price}</p>
+                  <p>{cartItems[item._id]}</p>
+                  <p>Rs.{item.price*cartItems[item._id]}</p>
+                  <p onClick={()=>removeCart(item._id)} className='cross'>x</p>
+                </div>
+              <hr></hr>
+              </div>
+              
+            )
+          }
+        })}
+
+
+
+      </div>
+
+      <div className="cart-bottom">
+        <div className="cart-total">
+          <h2>Cart Total</h2>
+          <div>
+            <div className="cart-total-details">
+              <p>Sub Total</p>
+              <p>Rs.{getTotalCartAmount()}</p>
+
+            </div>
+            <hr />
+
+            <div className="cart-total-details">
+              <p>Delivery Fee</p>
+              <p>Rs.{getTotalCartAmount()===0?0:2}</p>
+              
+            </div>
+            <hr />
+
+            <div className="cart-total-details">
+              <b>Total</b>
+              <b>Rs.{getTotalCartAmount()===0?0:getTotalCartAmount ()+2}</b>
+              
+            </div>
+          </div>
+          <button onClick={()=>navigate('/order')}>PROCEED TO CHECKOUT</button>
+        </div>
+
+        <div className="cart-promocode">
+          <div>
+            <p>If you have code, please enter it here</p>
+            <div className="cart-promocode-input">
+              <input type="text" placeholder='promo code' />
+              <button>Submit</button>
+            </div>
+          </div>
+        </div>
       </div>
 
 
